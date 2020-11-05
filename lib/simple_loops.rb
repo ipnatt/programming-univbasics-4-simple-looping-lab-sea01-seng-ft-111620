d1b517fb1a8232ab155ac3c4f7adc9e71b2227e2
Irvin Natt <ipnatt@gmail.com>
2020-11-05 09:17:51 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,12 +22,12 @@ while counter < array.length do
 end
 
 def return_string_array(array)
-  counter = 0
-  
-  while array[counter] do
-    puts array[counter]
-      counter += 1
-    end
+  count = 0
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s)
+    count += 1
   end
+  new_array
 end
-  
+
