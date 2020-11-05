470cb4d76a377ce1835c72aaafd04823a5952ca6
Irvin Natt <ipnatt@gmail.com>
2020-11-05 09:11:40 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -11,4 +11,12 @@ def loop_message_n_times(string, number)
     puts string
   end
 end
+
+def output_array(array)
+  counter = 0
   
+while counter < array.length do
+  puts array[counter]
+    counter += 1
+  end
+end
